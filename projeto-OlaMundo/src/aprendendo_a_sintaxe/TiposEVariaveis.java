0b6ddfa8330e36069d2a431b9cc8436bad8f841f
Elias boaventura <elias4144@hotmail.com>
2023-03-27 00:28:56 -0300

    Criado um novo pacote chamado aprendendo a sintaxe. Feito uma classe tipos e variaveis

---
+++ projeto-OlaMundo/src/aprendendo_a_sintaxe/TiposEVariaveis.java
@@ -0,0 +1,14 @@
+package aprendendo_a_sintaxe;
+
+public class TiposEVariaveis {
+
+	public static void main(String[] args) {
+		int valor1 = 10;
+		double valor2 = 12.50;
+		float vlaor3 = 12.5f;
+		boolean condicao1 = true;
+		String meuNome = "Elias Boaventura";
+
+	}
+
+}
